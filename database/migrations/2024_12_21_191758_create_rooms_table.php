888f811991808b124e4c8f218c7065d5c1287f7a
Gabriel Maximiniano <gabmaxsan@gmail.com>
2024-12-21 17:34:52 -0300

    fix: status type

--- database/migrations/2024_12_21_191758_create_rooms_table.php
+++ database/migrations/2024_12_21_191758_create_rooms_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('rooms', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('status');
+            $table->integer('status');
             $table->timestamps();
         });
     }
